a83ff39698e0b06b9454e6dd45c6ea48c66345d1
happywu <happywunaiping@gmail.com>
2017-03-03 14:06:37 +0800

    init

--- sym.py
+++ sym.py
@@ -10,11 +10,9 @@ def get_symbol_atari(act_dim):
     net = mx.symbol.Flatten(data=net)
     net = mx.symbol.FullyConnected(data=net, name='fc4', num_hidden=256)
     net = mx.symbol.Activation(data=net, name='relu4', act_type="relu")
-
     ## policy network
     fc_policy = mx.symbol.FullyConnected(data=net, name='fc_policy', num_hidden=act_dim)
     policy_network = mx.symbol.SoftmaxOutput(data=fc_policy, name='policy')
-
     ## value network
     fc_value = mx.symbol.FullyConnected(data=net, name='fc_value', num_hidden=1)
     value_network = mx.symbol.LinearRegressionOutput(data=fc_value, name='value')
