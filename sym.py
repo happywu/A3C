6db59a9b46843b8feaaf8105ea97ce5e58e05c32
happywu <happywunaiping@gmail.com>
2017-03-20 09:30:25 +0800

    add BN

--- sym.py
+++ sym.py
@@ -35,6 +35,45 @@ def get_symbol_atari(act_dim, entropy_beta=0.01):
 
     return mx.symbol.Group([policy_out, value_out, total_loss])
 
+def get_symbol_atari_bn(act_dim, entropy_beta=0.01):
+    data = mx.symbol.Variable('data')
+    net = mx.symbol.Cast(data=data, dtype='float32')
+    net = mx.symbol.Convolution(data=net, name='conv1', kernel=(8, 8), stride=(4, 4), num_filter=16)
+    net = mx.symbol.BatchNorm(data=net, name='bn1')
+    net = mx.symbol.LeakyReLU(data=net, name='leakyrelu1', act_type="leaky")
+    net = mx.symbol.Convolution(data=net, name='conv2', kernel=(4, 4), stride=(2, 2), num_filter=32)
+    net = mx.symbol.BatchNorm(data=net, name='bn2')
+    net = mx.symbol.LeakyReLU(data=net, name='leakyrelu2', act_type="leaky")
+    net = mx.symbol.Flatten(data=net)
+    net = mx.symbol.FullyConnected(data=net, name='fc4', num_hidden=256)
+    net = mx.symbol.BatchNorm(data=net, name='bn4')
+    net = mx.symbol.LeakyReLU(data=net, name='leakyrelu4', act_type='leaky')
+    ## policy network
+    fc_policy = mx.symbol.FullyConnected(data=net, name='fc_policy', num_hidden=act_dim)
+    policy = mx.symbol.SoftmaxActivation(data=fc_policy, name='policy')
+    policy_out = mx.symbol.BlockGrad(data=policy, name='policy_out')
+    ## value network
+    value = mx.symbol.FullyConnected(data=net, name='fc_value', num_hidden=1)
+    value_out = mx.symbol.BlockGrad(data=value, name='value_out')
+    # loss
+    rewardInput = mx.symbol.Variable('rewardInput')
+    actionInput = mx.symbol.Variable('actionInput')
+    tdInput = mx.symbol.Variable('tdInput')
+    # avoid NaN with clipping when pi becomes zero
+
+    policy_log = mx.symbol.log(mx.symbol.clip(data=policy, a_min=1e-20, a_max=1.0))
+    # add minus, because gradient ascend is used to optimize policy in the
+    # paper, here we use gradient descent
+    entropy =  - mx.symbol.sum(policy * policy_log, axis=1)
+
+    policy_loss = - mx.symbol.sum(mx.symbol.sum(policy_log * actionInput,
+                                                axis=1) * mx.symbol.sum(tdInput, axis=1) + entropy * entropy_beta)
+    value_loss = mx.symbol.sum(mx.symbol.square(rewardInput - value))
+    total_loss = mx.symbol.MakeLoss(policy_loss + (0.5 * value_loss))
+
+    return mx.symbol.Group([policy_out, value_out, total_loss])
+
+
 def get_symbol_forward(act_dim):
     data = mx.symbol.Variable('data')
     net = mx.symbol.Cast(data=data, dtype='float32')
@@ -55,6 +94,29 @@ def get_symbol_forward(act_dim):
 
     return mx.symbol.Group([policy_out, value_out])
 
+
+def get_symbol_forward_bn(act_dim):
+    data = mx.symbol.Variable('data')
+    net = mx.symbol.Cast(data=data, dtype='float32')
+    net = mx.symbol.Convolution(data=net, name='conv1', kernel=(8, 8), stride=(4, 4), num_filter=16)
+    net = mx.symbol.BatchNorm(data=net, name='bn1')
+    net = mx.symbol.LeakyReLU(data=net, name='leakyrelu1', act_type="leaky")
+    net = mx.symbol.Convolution(data=net, name='conv2', kernel=(4, 4), stride=(2, 2), num_filter=32)
+    net = mx.symbol.BatchNorm(data=net, name='bn2')
+    net = mx.symbol.LeakyReLU(data=net, name='leakyrelu2', act_type="leaky")
+    net = mx.symbol.Flatten(data=net)
+    net = mx.symbol.FullyConnected(data=net, name='fc4', num_hidden=256)
+    net = mx.symbol.BatchNorm(data=net, name='bn4')
+    net = mx.symbol.LeakyReLU(data=net, name='leakyrelu4', act_type='leaky')
+    ## policy network
+    fc_policy = mx.symbol.FullyConnected(data=net, name='fc_policy', num_hidden=act_dim)
+    policy = mx.symbol.SoftmaxActivation(data=fc_policy, name='policy')
+    policy_out = mx.symbol.BlockGrad(data=policy, name='policy_out')
+    ## value network
+    value = mx.symbol.FullyConnected(data=net, name='fc_value', num_hidden=1)
+    value_out = mx.symbol.BlockGrad(data=value, name='value_out')
+
+    return mx.symbol.Group([policy_out, value_out])
 
 def get_dqn_symbol(act_dim, ispredict=False):
     data = mx.symbol.Variable('data')
